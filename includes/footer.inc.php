2e171cecdbd66e4048e486fadfdab5fa775fb08d
Your Name <you@example.com>
2018-08-02 22:09:33 -0500

    footer.inc.php

---
+++ includes/footer.inc.php
@@ -0,0 +1,44 @@
+<!--Here is footer-->
+      <footer>
+        <div>
+          <ul>
+            <li><a href="#"><b>Main</b></a></li>
+            <li><a href="#">Flights</a></li>
+            <li><a href="#">Hotels</a></li>
+            <li><a href="#">Restaurants</a></li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li ><a href="#"><b>Service</b></a></li>
+            <li><a href="#">Insurance</a></li>
+            <li><a href="#">Language</a></li>
+            <li><a href="#">Transportation</a></li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li><a href="#"><b>About us</b></a></li>
+            <li><a href="#">Wind Travel</a></li>
+            <li><a href="#">History</a></li>
+            <li><a href="#">Stories</a></li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li><a href="#"><b>Links</b></a></li>
+            <li><a href="#">Goverment of Canada</a></li>
+            <li><a href="#">Destination Canada</a></li>
+            <li><a href="#">Tourism Winnipeg</a></li>
+          </ul>
+        </div>
+        <div id="social">
+        <div class="social"><a href="#"><img src="images/facebook.png" alt="facebook" width="35" height="35"/></a></div>
+        <div class="social"><a href="#"><img src="images/twitter.png" alt="twitter" width="35" height="35"/></a></div>
+        <div class="social"><a href="#"><img src="images/wechat.png" alt="wechat" width="35" height="35"/></a></div>  
+        </div>
+      </footer>
+      
+    </div>
+  </body>
+</html>
